9519633a71389270fe077357f21f8645e6f5de4b
Regnator <legendwyvernkiller@googlemail.com>
2015-03-31 11:17:39 +0200

    Logs and Filehandler implemented but not ready yet

--- PP2/src/de/regnator/pp/Rechner.java
+++ PP2/src/de/regnator/pp/Rechner.java
@@ -2,7 +2,11 @@ package de.regnator.pp;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 
 
@@ -18,43 +22,52 @@ public class Rechner implements Calculator {
 	public int add(int zahl1, int zahl2) {
 		int ergebniss;
 		ergebniss = zahl1 + zahl2;
+		log.finest("Das Ergebnis ist " + ergebniss);
 		return ergebniss;
 	}
-
 	
 	public int subtract(int zahl1, int zahl2) {
 		int ergebniss;
 		ergebniss = zahl1 - zahl2;
+		log.finest("Das Ergebnis ist " + ergebniss);
 		return ergebniss;
 	}
-
 	
 	public int multiply(int zahl1, int zahl2) {
 		int ergebniss;
 		ergebniss = zahl1 * zahl2;
+		log.finest("Das Ergebnis ist " + ergebniss);
 		return ergebniss;
 	}
-
 	
 	public float divide(int zahl1, int zahl2) {
-		float ergebniss;
-		ergebniss = zahl1 / zahl2;
+		if(zahl2==0){
+			log.severe("Teilen durch 0 ist nicht erlaubt ");
+			return 0;
+		}
+		float ergebniss = (float)zahl1 / (float)zahl2;
+		log.finest("Das Ergebnis ist " + ergebniss);
 		return ergebniss;
 	}
+	
 	public static void main(String[] args){
 		setuplogger();
-		log.info("Info");
-		log.warning("Warnung");
-		log.fine("Fine");
-		log.finer("Finer");
-		log.finest("Finest");
-		
+		Rechner t1 = new Rechner();
+		t1.add(1, 2);
+		t1.subtract(1, 2);
+		t1.multiply(1, 2);
+		t1.divide(10, 3);
 	}
+	
 	private static void setuplogger(){
 		final InputStream is = Rechner.class
 				.getResourceAsStream("log.properties");
 				try{
 					LogManager.getLogManager().readConfiguration(is);
+					
+					Handler fileHandler = new FileHandler("PPlogs.log",1000,5);
+					fileHandler.setFormatter(new SimpleFormatter());
+					log.addHandler(fileHandler);
 				}
 				catch(final IOException e){
 					Logger.getAnonymousLogger().severe("Could not load deafult logging.properties file");
